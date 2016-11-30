eb68129a633c07dbace18f5355567128931cc339
Gabriel Gellner <gabrielgellner@gmail.com>
2016-11-30 15:21:53 -0700

    Fixed up ellipsis calculations to use the correct entries

--- examples/circlaw.jl
+++ examples/circlaw.jl
@@ -1,6 +1,6 @@
 using Distributions
 using FoodWebs
-using PyPlot
+using OdinSon
 
 """
 # TODO: shuffling really matters to the goodness of fit ... why?
@@ -9,9 +9,26 @@ be over the entire distribution, as the current formula assumes equal marginal
 distributions). This is because the formula's used assume that the marginal distributions
 are equal (that is the distribution of the upper triangle and lower triangle are the same).
 I wonder how far just making sure that ρ is calculated from the actual pair distribution
-would work. (It works much better, though if the scaling is skewed, (lets say f = 0.1) the
-distribution can become wonky again)
+would work. (It works much better, though if the scaling is skewed, (lets say `f` = 0.1) the
+distribution can become wonky again).
+
+Even more interesting is that for low values of `f` we get a better fit with the naive
+formula, whereas for higher values of `f` the empirical formula does better. This suggests
+that something clear most be happening to the underlying parameters that I could try to
+integrate.
 """
 
-cmat = random_predprey(may_network(500, 0.25), Uniform(0, 5), 0.5, shuffle_prey=false)
-FoodWebs.plot_eigdist(cmat)
+cmat1 = random_predprey(may_network(500, 0.25), Uniform(0, 5), 0.1, shuffle_prey=false)
+cmat2 = random_predprey(may_network(500, 0.25), Uniform(0, 5), 0.4, shuffle_prey=false)
+cmat3 = random_predprey(may_network(500, 0.25), Uniform(0, 5), 0.8, shuffle_prey=false)
+cmat4 = random_predprey(may_network(500, 0.25), Uniform(0, 5), 1.0, shuffle_prey=false)
+
+figure()
+subplot(221)
+FoodWebs.plot_eigdist(cmat1)
+subplot(222)
+FoodWebs.plot_eigdist(cmat2)
+subplot(223)
+FoodWebs.plot_eigdist(cmat3)
+subplot(224)
+FoodWebs.plot_eigdist(cmat4)
--- src/circular_laws.jl
+++ src/circular_laws.jl
@@ -6,7 +6,6 @@ immutable EllipsisLawResult
     μ_diag::Float64
     σ2::Float64
     ρ::Float64
-    ρ2::Float64 # This is for the analytic formula (μ2 - μ^2)/σ2
     x_semi_axis::Float64
     y_semi_axis::Float64
 end
@@ -14,14 +13,14 @@ end
 """
     offdiag_pairs(mat::Matrix)
 
-return an array of the pairs of the offdiagonal pairs of the matrix `mat`
+return an array of the pairs of the offdiagonal pairs of the square matrix `mat`
 
 """
 function offdiag_pairs(mat::Matrix)
-    # Allocate a matrix of columns [a_ij, a_ji] excluding the diagonals
-    # the size is simply (S^2 - S)/2 = S(S - 1)/2
     S = size(mat, 1)
     @assert S == size(mat, 2) # must be square
+    # Allocate a matrix of columns [a_ij, a_ji] excluding the diagonals
+    # the size is simply (S^2 - S)/2 = S(S - 1)/2
     pairs = zeros(eltype(mat), Int(S*(S - 1)/2), 2)
     #TODO: likely there is a more efficient way of doing this? Can I skip over the diagonals
     # without an if statement?
@@ -39,51 +38,38 @@ function offdiag_pairs(mat::Matrix)
 end
 
 function ellipsis_sample(mat::Matrix{Float64})
-    @assert size(mat, 1) == size(mat, 2)
     S = size(mat, 1)
-    # all calculations need to ignore the diagonal
-    #imat = copy(mat) #TODO: is this the best way to do this?
-    #imat[diagind(imat)] = 0.0
-    # try to calc everyting in a single pass of the matrix
-    μ = 0.0
-    μ2 = 0.0
-    μ_diag = 0.0
-    σ2 = 0.0
-    for i = 1:S
-        for j = i:S
-            if i != j
-                μ += mat[i, j] + mat[j, i]
-                μ2 += mat[i, j]*mat[j, i]
-                σ2 += mat[i, j]^2 + mat[j, i]^2
-            else
-                μ_diag += mat[i, i]
-            end
-        end
-    end
-    μ = μ/(S*(S - 1))
-    μ2 = 2*μ2/(S*(S - 1))
-    μ_diag = μ_diag/S
-
-    # this formula assumes that the marginal distributions of off diagonal pairs are equal
-    # (which is why there is a single σ2)
-    #ρ0 = (μ2 - μ^2)/σ2
+    @assert S == size(mat, 2)
 
     pairs = offdiag_pairs(mat)
-    # Instead deal with the numerical covariance matrix of the off diagonal pairs
-    σ11, ρ21, σ22 = cov(pairs, corrected=false)[[1, 2, 4]]
-    #σ2 = σ2/(S*(S - 1)) - μ^2
-    #σ2 = mean([σ1, σ2]) # take the mean if the marginals are not equal? Is this a good rule?
-    σ2 = mean([σ11, σ22])
-    ρ = ρ21/sqrt(σ11*σ22)
-    ρ2 = (μ2 - μ^2)/σ2
-    # So instead we use the actual pair distribution to calculate this
-    #ρ2 = mean((pairs[:, 1] - mean(pairs[:, 1])).*(pairs[:, 2] - mean(pairs[:, 2])))/sqrt(σ11*σ22)
-    #ρ3 = cor(pairs)[1, 2] #TODO: I am not sure if this is using a sample formula or population
-    #@show ρ, ρ2, ρ3
+    μx = mean(pairs[:, 1])
+    μy = mean(pairs[:, 2])
+    μ = mean([μx, μy]) # this is the same as mean(pairs[:]), that is, the mean of the pair distribution
+    μxy = mean(pairs[:, 1].*pairs[:, 2])
+    μ_diag = mean(diag(mat))
 
+    # Instead deal with the numerical covariance matrix of the off diagonal pairs
+    σ2x, ρ21, σ2y = cov(pairs, corrected=false)[[1, 2, 4]]
+    # take the mean if the marginals are not equal? Is this a good rule?
+    #σ2 = mean([σ2x, σ2y])
+    # I for some reason feel that it should be the geometric mean, as I see the variance
+    # being multiplicative for some reason, note this is also the denominator of the correllation
+    σ2 = sqrt(σ2x*σ2y)
+    ρ = ρ21/sqrt(σ2x*σ2y)
+    #ρ3 = (μxy - μx*μy)/sqrt(σ2x*σ2y) same as ρ from scaled cov function, like above
+
+    #TODO: now that I am using the geometric mean of var, I am assumign that this makes sense
+    # in the sqrt(S*V) part
     x_semi_axis = sqrt(S*σ2)*(1 + ρ)
     y_semi_axis = sqrt(S*σ2)*(1 - ρ)
-    return EllipsisLawResult(S, μ, μ2, μ_diag, σ2, ρ, ρ2, x_semi_axis, y_semi_axis)
+
+    #return EllipsisLawResult(S, μx, μy, μ, μxy, μ_diag, σ2x, σ2y, σ2, ρ, ρ2, x_semi_axis, y_semi_axis)
+    return Dict(
+        :S => S,
+        :μx => μx, :μy => μy, :μxy => μxy, :μ => μ, :μ_diag => μ_diag,
+        :σ2x => σ2x, :σ2y => σ2y, :σ2 => σ2,
+        :ρ => ρ
+    )
 end
 
 #=
@@ -103,31 +89,17 @@ function plot_eigs(mat)
     #xlim(1.1*minimum(reigs), 1.1*maximum(reigs))
 end
 
-function eig_ellipse1(mat)
+function eig_ellipse(mat)
     ellps = ellipsis_sample(mat)
     Θs = linspace(π/2.0, 0.0, 500)
-    xbase = ellps.x_semi_axis*cos(Θs)
-    ybase = ellps.y_semi_axis*sin(Θs)
-    #TODO: add a commment as to why I am doing this
-    x = vcat(xbase, reverse(xbase), -xbase, reverse(-xbase))
-    y = vcat(ybase, reverse(-ybase), -ybase, reverse(ybase))
-    pelip = patches.Polygon(hcat(x + ellps.μ + ellps.μ_diag, y), fill=false, edgecolor="#0072B2", linewidth=2)
-    #TODO: should really pass this in
-    ax = gca()
-    ax[:add_patch](pelip)
-end
-
-function eig_ellipse2(mat)
-    ellps = ellipsis_sample(mat)
-    Θs = linspace(π/2.0, 0.0, 500)
-    x_semi_axis = sqrt(ellps.S*ellps.σ2)*(1 + ellps.ρ2)
-    y_semi_axis = sqrt(ellps.S*ellps.σ2)*(1 - ellps.ρ2)
+    x_semi_axis = sqrt(ellps[:S]*ellps[:σ2])*(1 + ellps[:ρ])
+    y_semi_axis = sqrt(ellps[:S]*ellps[:σ2])*(1 - ellps[:ρ])
     xbase = x_semi_axis*cos(Θs)
     ybase = y_semi_axis*sin(Θs)
     #TODO: add a commment as to why I am doing this
     x = vcat(xbase, reverse(xbase), -xbase, reverse(-xbase))
     y = vcat(ybase, reverse(-ybase), -ybase, reverse(ybase))
-    pelip = patches.Polygon(hcat(x + ellps.μ + ellps.μ_diag, y), fill=false, edgecolor="#A372B2", linewidth=2)
+    pelip = patches.Polygon(hcat(x + ellps[:μ] + ellps[:μ_diag], y), fill=false, edgecolor="#0072B2", linewidth=2)
     #TODO: should really pass this in
     ax = gca()
     ax[:add_patch](pelip)
@@ -135,6 +107,5 @@ end
 
 function plot_eigdist(mat)
     plot_eigs(mat)
-    eig_ellipse1(mat)
-    eig_ellipse2(mat)
+    eig_ellipse(mat)
 end
