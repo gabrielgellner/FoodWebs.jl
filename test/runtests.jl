7d96d54a1f9cc4a5f3d5ffa0241f0a7083750e8e
Gabriel Gellner <gabrielgellner@gmail.com>
2018-11-01 12:50:51 -0400

    Add some more tp tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,8 @@
 using FoodWebs
 using Test
 
+# Test set for trophic levels
 linchain = [0 1 0; 0 0 1; 0 0 0]
-trophic_levels(linchain)
 @test trophic_levels(linchain) ≈ [1.0, 2.0, 3.0]
 
 omnchain = [0 1 1; 0 0 1; 0 0 0]
@@ -15,6 +15,13 @@ omn_relabel = [0 0 1; 1 0 1; 0 0 0]
 cannibal = [1 1 0; 0 1 1; 0 0 1]
 @test trophic_levels(cannibal) ≈ [1.0, 2.0, 3.0]
 
+coupled_chain = [0 0 0 0 0; 0 0 1 0 0; 1 0 0 0 0; 1 0 0 0 0; 0 0 0 1 0]
+@test trophic_levels(coupled_chain) ≈ [3, 1, 2, 2, 1]
+
+coupled_chain_omn = [0 0 0 0 0; 1 0 1 0 0; 1 0 0 0 0; 1 0 0 0 0; 0 0 0 1 0]
+@test trophic_levels(coupled_chain_omn) ≈ [8 / 3, 1, 2, 2, 1]
+
+# Test set for connectance
 @test isapprox(connectance(may_network(500, 0.25)), 0.25, rtol = 1e-2)
 @test isapprox(connectance(may_network(500, 0.53)), 0.53, rtol = 1e-2)
 @test isapprox(connectance(may_network(500, 0.72)), 0.72, rtol = 1e-2)
@@ -26,3 +33,4 @@ cannibal = [1 1 0; 0 1 1; 0 0 1]
 @test isapprox(connectance(niche_network(500, 0.11)), 0.11, rtol = 1e-2)
 @test isapprox(connectance(niche_network(501, 0.67)), 0.67, rtol = 1e-2)
 @test isapprox(connectance(niche_network(502, 0.81)), 0.81, rtol = 1e-2)
+connectance(niche_network(501, 0.67))
