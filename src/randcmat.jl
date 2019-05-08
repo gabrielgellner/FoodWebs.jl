28b115fa9c09fe81de937975becdbfddcac0960b
Gabriel Gellner <gabrielgellner@gmail.com>
2019-05-08 14:58:59 -0400

    Fix typo

--- src/randcmat.jl
+++ src/randcmat.jl
@@ -1,7 +1,7 @@
 #TODO: I need to think of a better name for this.
 function random_predprey(pmat::PredationMatrix, dist::Distribution, f::Float64; shuffle_prey = false)
     #TODO: allow the diagonals to be set from an passed in argument
-    cmat = Matrix{Float64}(I, pmat.S, pmat.S)
+    cmat = -Matrix{Float64}(I, pmat.S, pmat.S)
     for i in 1:pmat.S, j in (i + 1):pmat.S
         if pmat[i, j] == 1
             neg = -abs(rand(dist))
